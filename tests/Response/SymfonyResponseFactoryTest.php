91f4a6064407048e71a49ec638c37e2ebd3be822
Gwendolen Lynch <gwendolen.lynch@gmail.com>
2021-12-18 13:56:00 +0100

    Add types, fix CS

--- tests/Response/SymfonyResponseFactoryTest.php
+++ tests/Response/SymfonyResponseFactoryTest.php
@@ -1,29 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Camelot\Intervention\Tests\Response;
 
-use League\Glide\Responses\SymfonyResponseFactory;
+use Camelot\Intervention\Response\SymfonyResponseFactory;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
-class SymfonyResponseFactoryTest extends TestCase
+/**
+ * @internal
+ * @covers \Camelot\Intervention\Response\SymfonyResponseFactory
+ */
+final class SymfonyResponseFactoryTest extends TestCase
 {
-    public function tearDown(): void
+    protected function tearDown(): void
     {
         Mockery::close();
     }
 
     public function testCreateInstance(): void
     {
-        self::assertInstanceOf(
-            'League\Glide\Responses\SymfonyResponseFactory',
-            new SymfonyResponseFactory()
-        );
+        static::assertInstanceOf(SymfonyResponseFactory::class, new SymfonyResponseFactory());
     }
 
     public function testCreate(): void
     {
-        $cache = Mockery::mock('League\Flysystem\FilesystemOperator', function ($mock) {
+        $cache = Mockery::mock('League\Flysystem\FilesystemOperator', function ($mock): void {
             $mock->shouldReceive('mimeType')->andReturn('image/jpeg')->once();
             $mock->shouldReceive('fileSize')->andReturn(0)->once();
             $mock->shouldReceive('readStream');
@@ -32,10 +35,10 @@ class SymfonyResponseFactoryTest extends TestCase
         $factory = new SymfonyResponseFactory();
         $response = $factory->create($cache, '');
 
-        self::assertInstanceOf('Symfony\Component\HttpFoundation\StreamedResponse', $response);
-        self::assertEquals('image/jpeg', $response->headers->get('Content-Type'));
-        self::assertEquals('0', $response->headers->get('Content-Length'));
-        self::assertStringContainsString(gmdate('D, d M Y H:i', strtotime('+1 years')), $response->headers->get('Expires'));
-        self::assertEquals('max-age=31536000, public', $response->headers->get('Cache-Control'));
+        static::assertInstanceOf('Symfony\Component\HttpFoundation\StreamedResponse', $response);
+        static::assertSame('image/jpeg', $response->headers->get('Content-Type'));
+        static::assertSame('0', $response->headers->get('Content-Length'));
+        static::assertStringContainsString(gmdate('D, d M Y H:i', strtotime('+1 years')), $response->headers->get('Expires'));
+        static::assertSame('max-age=31536000, public', $response->headers->get('Cache-Control'));
     }
 }
